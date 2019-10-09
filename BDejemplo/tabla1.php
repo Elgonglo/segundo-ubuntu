6264162cd418c463170bfa0c061dc2c2fbcb6d46
Unknown <gonzalo.30812@gmail.com>
2019-10-09 10:15:10 +0200

    BBDD update 1.0

---
+++ BDejemplo/tabla1.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: migue
+ * Date: 25/08/16
+ * Time: 16:02
+ */
+
+namespace Equipos;
+class equipos
+{
+    const NOMBRE = "nombre";
+    const NUM_JUGADORES = "Num jugadores";
+
+}
---
+++ BDejemplo/table2.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: migue
+ * Date: 25/08/16
+ * Time: 16:13
+ */
+
+namespace Equipos;
+
+
+class jugadores
+{
+    const EQUIPO = "equipo";
+    const NAME = "name";
+    const COLUMN_2 = "column_2";
+
+}
